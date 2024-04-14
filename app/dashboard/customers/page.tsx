8f4f3c591947ebd48f02744c94cbeed8a66d8f71
Juan Carranza <juancarranza7561@gmail.com>
2024-04-14 11:00:38 -0500

    Add new dashboard pages and layout components

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function CsutomersDashboardPage() {
+    return <p>This is just some text that we put in the customers dashboard page</p>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function InvoicesDashboardPage() {
+    return <p>Invoices dashboard page</p>
+}
